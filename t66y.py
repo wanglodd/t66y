403858355a23086ffd1d9d3512bd25119d6f1b22
Tony Chen <tony.chen910@gmail.com>
2015-11-11 22:59:48 +0800

    Add URL

--- t66y.py
+++ t66y.py
@@ -5,18 +5,32 @@ from bs4 import BeautifulSoup
 import re
 import sys
 
-lai = sys.argv[1]
+your_page = input('Please enter which page you would like to scan: ')
 
-URL = 'http://t66y.com/htm_data/15/1511/1713256.html'
+
+BASE_URL = 'http://t66y.com/thread0806.php?fid=15&search=&page='
+URL = BASE_URL + your_page.rstrip()
+
+def getpage(a):
+	laoshi_list = []
+	r = requests.get(a)
+	html = re.findall('htm_data(/[0-9]+/[0-9]+/[0-9]+\.html)', r.text)
+	for i in html:
+		c = 'http://t66y.com/htm_data' + i
+		laoshi_list.append(c)
+#	print(laoshi_list)
+	return laoshi_list
+
 
 def gethash(a):
-	r = requests.get(a)
-	hash = re.findall('hash=([0-9a-z]+)',r.text)
-	b = set(hash)
-	f_url = 'http://www.rmdown.com/link.php?hash=' + b.pop()
-	print(f_url)
-	return f_url
-#	if r.status_code == 200:
-#		soup = BeautifulSoup(r.text, "lxml")
+	for link in a:
+		r = requests.get(link)
+		hash = re.findall('hash=([0-9a-z]+)',r.text)
+		b = set(hash)
+		f_url = 'http://www.rmdown.com/link.php?hash=' + b.pop()
+		print(f_url)
+		return f_url
 
-gethash(lai)
+
+test = getpage(URL)
+gethash(test)
